0394f90179e2b780cb664cb3d7e179c563e92e73
dviladrich95 <dviladrich95@gmail.com>
2021-01-24 14:55:19 +0100

    debugging, all functions are from the helper file

---
+++ Exercise04_help/cmake-build-debug/CMakeFiles/main_automatic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_automatic.dir/main_automatic.cpp.o"
+  "main_automatic"
+  "main_automatic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_automatic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Exercise04_help/cmake-build-debug/CMakeFiles/main_manual.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_manual.dir/main_manual.cpp.o"
+  "main_manual"
+  "main_manual.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_manual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
